58708caf8805438e2f106fc78cae4779404aaeb1
tudorg <tudorg@indsoft.ro>
2023-10-10 15:33:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace Curs4Prob1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mesaj("High", "High");
+            Mesaj("Warning", "Warning");
+            Mesaj("Info", "Info");
+            Mesaj("Debug", "Debug");
+        }
+        public static void Mesaj(string severity, string message)
+        {
+            ConsoleColor textColor;
+
+            switch (severity.ToLower())
+            {
+                case "high":
+                    textColor = ConsoleColor.Red;
+                    break;
+                case "warning":
+                    textColor = ConsoleColor.Yellow;
+                    break;
+                case "info":
+                    textColor = ConsoleColor.White;
+                    break;
+                case "debug":
+                    textColor = ConsoleColor.Gray;
+                    break;
+                default:
+                    textColor = ConsoleColor.Gray;
+                    break;
+            }
+
+            Console.ForegroundColor = textColor;
+            Console.WriteLine($"{severity}: {message}");
+            Console.ResetColor();
+        }
+    }
+
+}
